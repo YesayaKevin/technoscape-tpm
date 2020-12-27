7f51239e9c9dfc84df16e3b7720793640b3311ce
Jason Sebastian <jason.sulistyawan@binus.ac.id>
2020-12-27 20:49:25 +0700

    added carousel for navbar

--- script/script.js
+++ script/script.js
@@ -21,3 +21,14 @@ $('.multiple-items').slick({
     autoplayspeed: 1500,
     cssEase: "ease-in-out",
   });
+
+  $('.multiple-items3').slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    dots: false,
+    arrows: false,
+    autoplay: true,
+    autoplayspeed: 1500,
+    cssEase: "ease-in-out",
+  });
