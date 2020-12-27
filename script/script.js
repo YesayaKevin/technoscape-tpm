22a98fb3f8fbdd141a5ed27939529749ab7adc74
Jason Sebastian <jason.sulistyawan@binus.ac.id>
2020-12-27 17:23:45 +0700

    added carousel, sponsor logo harus diset manual satu satu

---
+++ script/script.js
@@ -0,0 +1,17 @@
+// carousel
+
+$('.multiple-items').slick({
+    infinite: true,
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    dots: false,
+    arrows: false,
+  });
+  
+  $('.multiple-items2').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    dots: false,
+    arrows: false,
+  });
