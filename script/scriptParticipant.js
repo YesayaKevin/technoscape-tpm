c498570adea021ad6bed6802232bd8fa6aeb1baa
Yesaya Kevin <yeskev24@gmail.com>
2021-01-01 15:20:06 +0700

    admin & user done

--- script/scriptParticipant.js
+++ script/scriptParticipant.js
@@ -1,34 +1,14 @@
-const popUpPaymentStatus = document.getElementById('popUpPaymentStatus')
-const closeStatus = document.getElementById('closeStatus')
-const openStatus = document.getElementById('statusPopUpToggle')
-const closeStatus1 = document.getElementById('closeStatus1')
-
-openStatus.addEventListener("click", openStatusPopUp)
-closeStatus.addEventListener("click", closeStatusPopUp)
-closeStatus1.addEventListener("click", closeStatusPopUp)
-
-function closeStatusPopUp() {
-    const popUpContents = document.getElementsByClassName("popUp-Status")
-    for (content of popUpContents) {
-        content.classList.toggle("hidden", true)
-    }
-    popUpPaymentStatus.classList.toggle("hidden", true)
-}
-
-function openStatusPopUp() {
-    const target = document.getElementById(this.dataset.target)
-    target.classList.toggle("hidden", false)
-    popUpPaymentStatus.classList.toggle("hidden", false)
-}
-
 $(document).ready(function () {
-    $("#popUpPaymentStatus").hide();
-
-    $(".buttonZ").click(function () {
-        $("#popUpPaymentStatus").show();
-    });
-
-    $("#popUpPaymentStatus").click(function () {
-        $(this).hide();
-    });
-});
+    $('.delete-pop-up-btn').on('click', function(){
+        $(this).siblings('.delete-pop-up').first().toggleClass('hidden', false)
+        $('#delete-pop-up-bg').toggleClass('hidden', false)
+    })
+    $('.cancel-btn').on('click', function(){
+        $(this).parents('.delete-pop-up').toggleClass('hidden', true)
+        $('#delete-pop-up-bg').toggleClass('hidden', true)
+    })
+    $('.delete-btn').on('click', function(){
+        $(this).parents('.delete-pop-up-card').remove()
+        $('#delete-pop-up-bg').toggleClass('hidden', true)
+    })
+})
